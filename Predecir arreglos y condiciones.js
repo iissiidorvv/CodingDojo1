5c7b0a5b8924c631b8db9253337b5f47c715b01f
iissiidorvv <101187473+iissiidorvv@users.noreply.github.com>
2022-04-12 10:47:36 -0400

    Add files via upload

---
+++ Predecir arreglos y condiciones.js
@@ -0,0 +1,40 @@
+fragmento 1=
+*/
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+/*
+variable           Datos
+auntContacInfo    ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+
+*/
+
+fragmento 2=
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+
+/*
+variable       Datos
+produce        ["manzanas", "naranjas"];
+frozen         ["brócoli", "helado"];   
+
+*/
+
+fragmento 3=
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+/*
+variable        Datos
+movieLibrary   ["Bambi", "E.T.", "Toy Story"];
+*/
